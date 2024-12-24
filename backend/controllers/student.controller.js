7b3f403b290570f376f8734c75cfdd9a36a72685
toufiq007 <t.limon2342@gmail.com>
2024-12-25 00:21:56 +0600

    convert our image to base64 string and save this directly to the database by using multer.memoryStorage() function

--- backend/controllers/student.controller.js
+++ backend/controllers/student.controller.js
@@ -3,15 +3,18 @@ import { StudentModel } from "../models/student.models.js";
 const registerStudent = async (req, res) => {
   try {
     const { name, age, phone, email, address } = req.body;
-    console.log(req.file)
-    const photoPath = req.file.path
+    console.log(req.file);
+    // const photoPath = req.file.path
+    // convert to our photo to base64 string format and then save to db
+    const photoBase64 = req.file ? req.file.buffer.toString("base64") : null;
     const newStudent = await StudentModel.create({
       name,
       age,
       phone,
       email,
       address,
-      photo:photoPath
+      //   photo:photoPath
+      photo: photoBase64,
     });
     return res
       .status(200)
@@ -22,8 +25,6 @@ const registerStudent = async (req, res) => {
   }
 };
 
-
-
 export const studentController = {
-    registerStudent
-}
+  registerStudent,
+};
--- backend/middlewares/uploadFile.js
+++ backend/middlewares/uploadFile.js
@@ -2,20 +2,21 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const dir = "uploads";
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-    }
-    cb(null, dir);
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      Date.now() + "_" + file.fieldname + path.extname(file.originalname)
-    );
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     const dir = "uploads";
+//     if (!fs.existsSync(dir)) {
+//       fs.mkdirSync(dir);
+//     }
+//     cb(null, dir);
+//   },
+//   filename: (req, file, cb) => {
+//     cb(
+//       null,
+//       Date.now() + "_" + file.fieldname + path.extname(file.originalname)
+//     );
+//   },
+// });
 
+const storage = multer.memoryStorage()
 export const upload = multer({ storage });
