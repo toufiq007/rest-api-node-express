5c14b6608cf76a9472035f330f1e8bbd0fc7b9a4
toufiq007 <t.limon2342@gmail.com>
2025-01-03 21:56:23 +0600

    setup our backend file for prisma orm and install prisma and initialize prisma

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,12 @@
-import express from "express";
-import dotenv from "dotenv";
-import connectDb from "./db/db.config.js";
-import studentRoutes from "./routes/student.routes.js";
-import bodyParser from "body-parser";
-import cors from "cors";
-dotenv.config();
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express();
 
-// database connection
-connectDb();
-
-// middlewares
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-
-// for our static file --> this data will be accessed file client
-app.use(express.static("profile"));
-
-// router setup
-app.use("/api", studentRoutes);
-
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
-});
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
