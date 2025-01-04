7b546e29eee026d35c3c18ec9a4bc71a73296d99
toufiq007 <t.limon2342@gmail.com>
2025-01-04 11:36:12 +0600

    created user controller and userRoutes and setup routes in the index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 import express from "express"
 import dotenv from "dotenv"
+import userRoutes from "./routes/user.routes.js";
 dotenv.config()
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(express.json())
+
+// user routes
+app.use('/api',userRoutes)
 
 
 app.listen(port,()=>{
